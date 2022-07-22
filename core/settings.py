8b02947e89b6461951ba1836a7075286c1a0b1ab
madhur-mongia-sp <madhur.mongia@stackpro.in>
2022-07-22 16:23:27 +0530

    config static files

--- core/settings.py
+++ core/settings.py
@@ -142,13 +142,14 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 AUTH_USER_MODEL = 'accounts.newUser'
 
-STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
