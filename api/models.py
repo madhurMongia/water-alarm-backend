bdd51caa13d55bbb5043df454a3fb04522610d09
madhur-mongia-sp <madhur.mongia@stackpro.in>
2022-08-04 00:10:00 +0530

    changed level type

--- api/models.py
+++ api/models.py
@@ -25,7 +25,7 @@ class WaterLevel(models.Model):
 
     water_tank = models.ForeignKey(
         WaterTank, related_name="levels", on_delete=models.CASCADE)
-    level = models.DecimalField(max_digits=10, decimal_places=2)
+    level = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
